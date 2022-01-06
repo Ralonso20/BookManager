ab0102957ad3ec6e0689bbfc11e703a3194cf760
Rodri <rodrigoalonso2000@gmail.com>
2022-01-06 16:14:04 -0300

    Creado Modelo Interfaz

---
+++ ui_files/Ui_main_window.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'main_window.ui'
+##
+## Created by: Qt User Interface Compiler version 6.2.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QSizePolicy, QWidget)
+
+class Ui_Bookdepository(object):
+    def setupUi(self, Bookdepository):
+        if not Bookdepository.objectName():
+            Bookdepository.setObjectName(u"Bookdepository")
+        Bookdepository.resize(960, 550)
+
+        self.retranslateUi(Bookdepository)
+
+        QMetaObject.connectSlotsByName(Bookdepository)
+    # setupUi
+
+    def retranslateUi(self, Bookdepository):
+        Bookdepository.setWindowTitle(QCoreApplication.translate("Bookdepository", u"Library", None))
+    # retranslateUi
+
